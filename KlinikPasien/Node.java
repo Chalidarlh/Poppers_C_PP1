2e0c9e6bf242d16803a761218d51e72850edef37
DwiyanYusup <dwiyanyusup24@gmail.com>
2025-05-24 14:17:15 +0700

    usup

--- KlinikPasien/Node.java
+++ KlinikPasien/Node.java
@@ -2,4 +2,24 @@ package KlinikPasien;
 
 public class Node {
     private String nama;
+    private String alamat;
+    private String noTelp;
+    private String keluhan;
+    private Node next;
+    private Node prev;
+    private Node head;
+    private Node tail;
+    private int noAntrian;
+    private int noAntrianAwal;
+    private int noAntrianAkhir;
+    private int noAntrianSementara;
+
+    public Node(String nama, String alamat, String noTelp, String keluhan) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.noTelp = noTelp;
+        this.keluhan = keluhan;
+        this.next = null;
+        this.prev = null;
+    }
 }
