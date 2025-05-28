36a45b30729e3370855e5ce3f9b3cf8d2afa3025
nurfatimahh <nurfatimahhh1603z@gmail.com>
2025-05-28 11:27:00 +0700

    nodepasien

--- KlinikPasien/Pasien.java
+++ KlinikPasien/Pasien.java
@@ -5,25 +5,31 @@ public class Pasien {
     private String nama;
     private String jenisKelamin;
     private int usia;
+    private String nomorTelepon;
     private String alamat;
+    private String tanggalKunjungan;
     private String keluhan;
     private String statusPasien;
 
-    public Pasien(String nama, String jenisKelamin, int usia, String alamat, String keluhan, String statusPasien) {
+    public Pasien(String nama, String jenisKelamin, int usia,String nomorTelepon, String alamat, String tanggalKunjungan, String keluhan, String statusPasien) {
         this.nama = nama;
         this.jenisKelamin = jenisKelamin;
         this.usia = usia;
+        this.nomorTelepon = nomorTelepon;
         this.alamat = alamat;
+        this.tanggalKunjungan = tanggalKunjungan;
         this.keluhan = keluhan;
         this.statusPasien = statusPasien;
-
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getNama() {
         return nama;
     }
@@ -48,12 +54,28 @@ public class Pasien {
         this.usia = usia;
     }
 
+    public String getNomorTelepon() {
+        return nomorTelepon;
+    }
+
+    public void setNomorTelepon(String nomorTelepon) {
+        this.nomorTelepon = nomorTelepon;
+    }
+
     public String getAlamat() {
         return alamat;
     }
 
     public void setAlamat(String alamat) {
         this.alamat = alamat;
+    }
+
+    public String getTanggalKunjungan() {
+        return tanggalKunjungan;
+    }
+
+    public void setTanggalKunjungan(String tanggalKunjungan) {
+        this.tanggalKunjungan = tanggalKunjungan;
     }
 
     public String getKeluhan() {
