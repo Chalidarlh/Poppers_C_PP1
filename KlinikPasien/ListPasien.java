80199ec7d644e9b56052e0b2db9465882da0c5d1
RezaNrjaman <rezanrjaman121204@gmail.com>
2025-06-07 10:39:13 +0700

    io

--- KlinikPasien/ListPasien.java
+++ KlinikPasien/ListPasien.java
@@ -1,10 +1,6 @@
 package KlinikPasien;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDate;
 
 public class ListPasien {
